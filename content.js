48a7dab4c2d0ec33670090c18c51c1d47654dd97
Vicente Kalinoski Parmigiani <vicenteparmigiani@gmail.com>
2021-04-12 14:41:02 -0300

    Add files via upload

---
+++ content.js
@@ -0,0 +1,63 @@
+const content = [
+    {
+        "title": "Websites",
+        "description": "Páginas para a internet desenvolvidas em HTML, CSS, JavaScript etc. ao longo dos anos e para os mais diversos fins.",
+        "content": [
+            {
+                "card_title": "Gincana CAQuí",
+                "card_subtitle": "Desenvolvido para a recepção dos calouros do curso de Química com atividades visando a integração.",
+                "card_img": "gincana"
+            },{
+                "card_title": "Período Especial",
+                "card_subtitle": "Criado para organizar as informações de disciplinas ofertadas no curso de Química da UFPR durante a pandemia.",
+                "card_img": "gincana"
+            },{
+                "card_title": "Cardápio RU",
+                "card_subtitle": "Gera uma imagem pronta para a divulgação com o cardápio do Restaurante Universitário da UFPR",
+                "card_img": "gincana"
+            },{
+                "card_title": "Tabela Periódica",
+                "card_subtitle": "Feita para consulta e memorização de todos os elementos da tabela periódica.",
+                "card_img": "gincana"
+            },{
+                "card_title": "Primavera Cultural",
+                "card_subtitle": "Utilizado para o controle do áudio durante a apresentação do Projeto Primavera Cultural do Colégio Franciscano São José em 2017",
+                "card_img": "gincana"
+            },{
+                "card_title": "Minimal Planet",
+                "card_subtitle": "Website para ser utilizado como papel de parede, apresentando um planeta com suas luas de forma minimalista.",
+                "card_img": "gincana"
+            },{
+                "card_title": "BB-8",
+                "card_subtitle": "Apenas para diversão, onde é possível \"conversar\" com o robô BB-8, de Star Wars, além de controlá-lo.",
+                "card_img": "gincana"
+            },{
+                "card_title": "Currículo",
+                "card_subtitle": "Este próprio site.",
+                "card_img": "gincana"
+            }
+        ]
+    },{
+        "title": "Redes Sociais",
+        "description": "Páginas para a internet desenvolvidas em HTML, CSS, JavaScript etc. ao longo dos anos e para os mais diversos fins.",
+        "content": [
+            {
+                "card_title": "Gincana CAQuí",
+                "card_subtitle": "Desenvolvido para a recepção dos calouros do curso de Química em 2020.",
+                "card_img": "gincana"
+            },{
+                "card_title": "aaaaaaa PeriódicaA",
+                "card_subtitle": "Interativa para a memorização dos elementos.",
+                "card_img": "image2.png"
+            },{
+                "card_title": "aaaaaaa PeriódicaB",
+                "card_subtitle": "Interativa para a memorização dos elementos.",
+                "card_img": "image2.png"
+            },{
+                "card_title": "aaaaaaa PeriódicaC",
+                "card_subtitle": "Interativa para a memorização dos elementos.",
+                "card_img": "image2.png"
+            }
+        ]
+    }
+]
---
+++ script.js
@@ -0,0 +1,140 @@
+// Code by Vicente K. Parmigiani 2021
+
+// globalVariables
+
+let currentPage = 0;
+let cat = 0;
+let cardsPerPage = Math.floor((window.innerWidth - 220) / 352)-1;
+const contentHolder = document.getElementById("content");
+const infoPage = document.getElementById('info_page');
+const infoPageBg = document.getElementById('info_page_bg');
+let contentLength = 0;
+
+// Nav menu
+
+for(let i = 0; i <= 6; i++) {
+    const nav = document.getElementById("nav"+i);
+    nav.addEventListener("click", () => {
+        setPagePos(i);
+    })
+}
+
+function setPagePos(pos) {
+    const navBox = document.getElementById("selected_nav");
+    navBox.style.top = 190 + pos*40 + "px";
+    cat = pos;
+    inflateContent(pos);
+}
+
+// Buid website
+
+function inflateContent(pos) {
+    contentLength = Math.floor(Object.keys(content[cat].content).length / (cardsPerPage+1)) +1;
+    const dots = document.getElementById('dots');
+    dots.innerHTML = "";
+
+    for (i=0; i<contentLength; i++) {
+        const dot = document.createElement("div");
+        dot.className = "dot";
+        dot.id = "dot" + i;
+        dots.appendChild(dot);
+    }
+    
+    setPagePosRow(0);
+
+}
+
+function setPagePosRow(page) {
+    currentPage = page;
+    contentHolder.innerHTML = ""
+
+    contentHolder.preventDefault;
+    contentHolder.classList.remove("contentAnimation");
+    void contentHolder.offsetWidth;
+    contentHolder.classList.add("contentAnimation");
+
+    if (currentPage == 0) {
+        inflateDiv(0, 0);
+        for(let i=1; i <= cardsPerPage; i++) {
+            inflateDiv(content[cat].content[i-1], i-1);
+        }
+    } else {
+        for(let i=0; i < cardsPerPage+1; i++) {
+            try {
+            inflateDiv(content[cat].content[(cardsPerPage*page)+page+i-1], (cardsPerPage*page)+page+i-1);
+            } catch (e) {}
+        }
+    }
+
+    for (i=0;i < contentLength;i++) {
+        document.getElementById("dot"+i).className = "dot";
+    };
+
+    document.getElementById("dot"+page).className = "dot selecteddot"
+}
+
+function inflateDiv(divSettings, id) {
+    const div = document.createElement('div');
+    const subdiv = document.createElement('div');
+    const title = document.createElement('h2');
+    const subtitle = document.createElement('p');
+
+    if(divSettings == 0) {
+        div.className = "titleDiv";
+        subdiv.className = 'titlesubdiv';
+        title.innerHTML = content[cat].title;
+        title.className = "titletitle";
+        subtitle.innerHTML = content[cat].description;
+    } else {
+        div.className = 'div';
+        div.id = 'div.' + cat + '.' + id;
+        div.addEventListener('click', () => {
+            openDialog(cat, id);
+        })
+        subdiv.className = 'subdiv';
+        title.className = 'title';
+        title.innerHTML = divSettings.card_title;
+        subtitle.innerHTML = divSettings.card_subtitle;
+        div.style.backgroundImage = 'url(assets/'+divSettings.card_img+'.png)';
+    }
+
+    subdiv.appendChild(title);
+    subdiv.appendChild(subtitle);
+    div.appendChild(subdiv)
+    contentHolder.appendChild(div);
+}
+
+inflateContent(0);
+
+function openDialog(c, id) {
+    infoPage.className = "infopageAnimation";
+    infoPage.style.animationName = "infoIn";
+    infoPage.style.display = "block";
+
+    infoPageBg.style.display = "block";
+    infoPageBg.style.opacity = 1;
+
+    const title = document.getElementById('page_title');
+    const subtitle = document.getElementById('page_subtitle');
+    const text = document.getElementById('page_info');
+
+    title.innerHTML = content[c].content[id].card_title;
+    subtitle.innerHTML = content[c].content[id].card_subtitle;
+}
+
+function closeDialog() {
+    infoPage.style.animationName = "infoOut";
+    setTimeout(() => {infoPage.style.display = "none"},500);
+    infoPageBg.style.opacity = 0;
+    setTimeout(() => {infoPageBg.style.display = "none"},200);
+}
+
+document.getElementById("arrow_next").addEventListener("click", () => {
+    if (currentPage < contentLength-1) {
+        currentPage++;
+    } else {
+        currentPage = 0;
+    }
+
+    setPagePosRow(currentPage);
+})
